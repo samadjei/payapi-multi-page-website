2d7f1b2fdaf0693994e5aaf2c93c99af233c6a59
Samuel Adjei <info.samadjei@gmail.com>
2021-05-27 20:54:06 +0100

    first commit

--- components/layout.js
+++ components/layout.js
@@ -5,10 +5,12 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<Head>
-				<title>Title</title>
+				<title>Frontend Mentor | PayAPI Website Challenge</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" type="image/png" sizes="32x32" href="../public/favicon.ico"></link>
-				{/* <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet" /> */}
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Public+Sans:wght@400;700&display=swap" rel="stylesheet" />
 			</Head>
 			<div>
 				<Navbar />
--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../components/button';
 
 const Navbar = () => {
 	return (
-		<div>
-			<p>This is a navbar</p>
-		</div>
+		<nav className="navbar container">
+			<div className="navbar__left">
+				<div className="navbar__logo">
+					<Link href="/">
+						<Image src="/logo.svg" alt="Scoot logo" width={107.82} height={28.56} alt="Logo" />
+					</Link>
+				</div>
+				<div className="navbar__links">
+					<Link href="/about">
+						<a>About Us</a>
+					</Link>
+					<Link href="/location">
+						<a>Location</a>
+					</Link>
+					<Link href="/careers">
+						<a>Careers</a>
+					</Link>
+				</div>
+			</div>
+			<div className="cta">
+				<Link href="/">
+					<Button className="btn hero--btn" buttonStyle="btn--primary" buttonSize="btn--desktop">
+						Get Scootin
+					</Button>
+				</Link>
+			</div>
+		</nav>
 	);
 };
 
