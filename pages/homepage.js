c375c7e2fa2df6fd4903d03fc16e30d00ef69d1f
Samuel Adjei <info.samadjei@gmail.com>
2021-11-04 15:48:40 +0000

    updated the backgorund pattern on homepage

--- pages/homepage.js
+++ pages/homepage.js
@@ -3,10 +3,14 @@ import Hero from '../components/homepage/hero';
 import Offers from '../components/homepage/offers';
 import Subscription from '../components/homepage/subscription';
 import Work from '../components/homepage/work';
+import Image from 'next/image';
 
 const Homepage = () => {
 	return (
 		<main className="main">
+			<div className="bg-pattern">
+				<Image src="/assets/shared/desktop/bg-pattern-circle.svg" width={780} height={780} />
+			</div>
 			<Hero />
 			<Work />
 			<Features />
